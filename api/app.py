98399b260df2c2ddca6244d097061f1bf62ce4e7
Yasmeen34320 <y.alaa9.2001@gmail.com>
2024-04-20 02:54:59 +0200

    first commit

---
+++ api/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# # Set the path to Tesseract executable (update this with your actual Tesseract path)
+#pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+
+@app.route('/')
+def hello():
+    return "<h1>Hello, World!! </h1>"
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000)
+
