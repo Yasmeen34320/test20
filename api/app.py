cd83ac5ddfe988e64d785769d8df52a4c524baf4
yasmeen Alaa Taha <138386021+Yasmeen34320@users.noreply.github.com>
2024-04-20 03:04:43 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -11,6 +11,6 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello, World!! </h1>"
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
 
